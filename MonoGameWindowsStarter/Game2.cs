8de963ad1312aef128d63ddf7b584f0bd42b65fa
Blake Wewer <blake.wewer@yahoo.com>
2020-02-10 15:14:10 -0600

    Initialized Game2
    Set up and renamed to Game2

--- MonoGameWindowsStarter/Game2.cs
+++ MonoGameWindowsStarter/Game2.cs
@@ -7,12 +7,12 @@ namespace MonoGameWindowsStarter
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class Game2 : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public Game2()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- MonoGameWindowsStarter/Program.cs
+++ MonoGameWindowsStarter/Program.cs
@@ -14,7 +14,7 @@ namespace MonoGameWindowsStarter
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Game2())
                 game.Run();
         }
     }
